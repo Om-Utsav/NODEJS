4f2cffe9cf0b0c593cb9228494a3b4eb48040d65
Om-Utsav <86879928+Om-Utsav@users.noreply.github.com>
2024-05-24 12:04:51 +0530

    Update Week1.js

--- Week1.js
+++ Week1.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+//for printing hello world: 
+//console.log("Hello World")
+
+//for node.js application:
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.write('Hello World!');
+    res.end();
+} ) .listen(5000);
+
+console.log('Server running at 5000');
